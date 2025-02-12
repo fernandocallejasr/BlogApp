a322f98c981f1481b2d30bcdd2f078bd7b82230b
Fernando Callejas <ferd.callejas@gmail.com>
2025-02-11 19:37:58 -0600

    All blogs page and Individual blog page addition Adds All Blogs page - This page includes all blogs created so far by user - Page shows a shortened version of the blog in cards - User is able to go to the complete blog from this page Adds route handler for all blogs page - Renders EJS file with passed data about blogs for all blogs page - Renders EJS file for complete blog when redirected from all blogs page Adds middleware to handle request body

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const app = express();
 const PORT = 3000;
 const MAIN_FOLDER_NAME = dirname(fileURLToPath(import.meta.url));
 
+var blogs = [];
+
 // Middleware
 app.use(express.static(MAIN_FOLDER_NAME + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/app", (req, res) => {
     res.render("index.ejs");
@@ -19,10 +22,58 @@ app.get("/app/new-blog", (req, res) => {
     res.render("new-blog.ejs");
 });
 
+app.get("/app/all-blogs", (req, res) => {
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+    
+    res.render("all-blogs.ejs", {
+        allBlogs: blogs,
+        date: formattedDate
+    })
+});
+
 app.post("/app/postBlog", (req, res) => {
     console.log(req.body);
+    blogs.push(req.body);
 
-    res.send("Hola");
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    res.render("blog.ejs", {
+        blogTitle: req.body["blogTitle"],
+        blogContent: req.body["blogContent"],
+        newPost: true,
+        date: formattedDate
+    });
+});
+
+app.post("/app/blog", (req, res) => {
+    console.log(req.body);
+    // blogs.push(req.body);
+
+    let blogIndex = req.body["blogIndex"];
+    console.log("Blog at index " + blogIndex + ": " + blogs[blogIndex]["blogTitle"]);
+
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    res.render("blog.ejs", {
+        blogTitle: blogs[blogIndex]["blogTitle"],
+        blogContent: blogs[blogIndex]["blogContent"],
+        date: formattedDate
+    });
 });
 
 app.listen(PORT, () => {
